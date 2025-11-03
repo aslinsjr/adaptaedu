71526f76eae04f7b7b036d2a33c3ec6c222f87c2
Alexandre Lins <aslinsjr@gmail.com>
2025-11-03 08:05:15 -0300

    Ajustes endpoint-api

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ import PreferencesPanel from './components/PreferencesPanel.jsx';
 import ContentSidebar from './components/ContentSidebar.jsx';
 import './App.css';
 
-const API_URL = 'https://adaptaedu-qfixyy5jj-aslinsjrs-projects.vercel.app/api'
-
+const API_URL = process.env.API_URL
 function App() {
     const [conversationId, setConversationId] = useState(null);
     const [messages, setMessages] = useState([]);
